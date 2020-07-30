6cf287b02d0e55bccf5782e9ac519e7fe8458cb3
GGleyser <ggleyser@outlook.com>
2020-07-29 23:22:55 -0300

    sobre o curso: controller, model, factory, migration

--- database/factories/ModelContatoFactory.php
+++ database/factories/ModelContatoFactory.php
@@ -3,10 +3,13 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\ModelContato;
+use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
 $factory->define(ModelContato::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'telefone' => $faker->phoneNumber,
     ];
 });
--- database/factories/ModelProdutosFactory.php
+++ database/factories/ModelProdutosFactory.php
@@ -7,6 +7,8 @@ use Faker\Generator as Faker;
 
 $factory->define(ModelProdutos::class, function (Faker $faker) {
     return [
-        //
+        'titulo' => $faker->title,
+        'descricao' => $faker->paragraph,
+        'valor' => 100.00,
     ];
 });
